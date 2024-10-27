7dd11e229b49e8408599b59a40b8726c575dcdce
gmh5225 <2315157@qq.com>
2024-10-27 15:38:20 +0800

    fix cpuidex

--- cpuid_amd64.s
+++ cpuid_amd64.s
@@ -2,11 +2,12 @@
 
 // func cpuidex(leaf, subleaf uint32) (eax, ebx, ecx, edx uint32)
 TEXT ·cpuidex(SB), NOSPLIT, $0-24
-    MOVL leaf+0(FP), AX
-    MOVL subleaf+8(FP), CX
-    CPUID
-    MOVL AX, eax+16(FP)
-    MOVL BX, ebx+20(FP)
-    MOVL CX, ecx+24(FP)
-    MOVL DX, edx+28(FP)
+    MOVL    leaf+0(FP), AX      
+    MOVL    subleaf+4(FP), CX   
+    CPUID                       
+    MOVL    AX, ret+8(FP)      
+    MOVL    BX, ret+12(FP)     
+    MOVL    CX, ret+16(FP)     
+    MOVL    DX, ret+20(FP)     
     RET
+
